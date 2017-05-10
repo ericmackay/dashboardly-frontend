e3c1e34d6c5b3a0e64b8de447c6f6660a7be25d3
Your Name <you@example.com>
2017-05-10 11:42:02 -0400

    added a local host to api.js

--- src/api.js
+++ src/api.js
@@ -1,35 +1,36 @@
 import superagent from 'superagent'
 import { API_HOST } from './config'
+import { APT_HOST2} from './config'
 
 class Api {
-  
+
   requestLogin = (email, password) => (
     superagent
-    .post(`${API_HOST}/auth/sessions`)
+    .post(`${API_HOST2}/auth/sessions`)
     .send({ email, password })
   )
-  
+
   requestLogout = (token) => (
     superagent
-    .delete(`${API_HOST}/auth/sessions`)
+    .delete(`${API_HOST2}/auth/sessions`)
     .set('Authorization', `token ${token}`)
   )
-  
+
   getBoardsList = (page, count) => (
     superagent
-    .get(`${API_HOST}/boards`)
+    .get(`${API_HOST2}/boards`)
   )
-  
+
   getBoard = (id) => (
     superagent
-    .get(`${API_HOST}/boards/${id}`)
+    .get(`${API_HOST2}/boards/${id}`)
   )
-  
+
   getBookmarks = (boardId) => (
     superagent
-    .get(`${API_HOST}/boards/${boardId}/bookmarks`)
+    .get(`${API_HOST2}/boards/${boardId}/bookmarks`)
   )
-  
+
 }
 
 export default new Api();
